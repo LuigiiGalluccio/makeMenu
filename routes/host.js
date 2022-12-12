f8db1c2dfad962729a594eb378c72e09db644ba2
dangelo.simone <simone.dangelo001@studenti.uniparthenope.it>
2022-12-12 16:10:45 +0100

    commit Simone D'Angelo

--- routes/host.js
+++ routes/host.js
@@ -1,12 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var Account = require('../models/Account');
 
 /* GET home page. */
-router.get('/host.html', function(req, res, next) {
-    res.render('host', { title: 'Express' });
+router.get('/host.html', async function (req, res, next) {
     var id = req.get('id');
+    var user = await Account.findById(id);
+    if(user){
+        document.body.innerHTML = user.get('menu_saved');
+    }
+    res.render('host', {title: 'Express'});
 });
-
-
-
 module.exports = router;
