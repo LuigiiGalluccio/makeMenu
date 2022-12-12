6b587c1bff7852b264a738e02f8d49d685847d05
dangelo.simone <simone.dangelo001@studenti.uniparthenope.it>
2022-12-12 23:30:54 +0100

    commit Simone D'Angelo

--- models/Account.js
+++ models/Account.js
@@ -21,32 +21,12 @@ var AccountSchema = new mongoose.Schema({
         type: String,
         required: false
     },
-    menu:{
-        sections:[{
-            menu_id:{
-                type: String,
-                required: true
-            },
-            items:[{
-                title:{
-                    type: String,
-
-                },
-                description:{
-                    type: String,
-
-                },
-                price:{
-                    type: String,
-
-                },
-            }]
-
-        }],
-    },
     menu_saved:{
         type: String,
-    }
+    },
+    menu_image:{
+        type: Buffer,
+    },
 });
 
 
